7b943c4256a401892de95e2a60787c2a94befe27
Ghawsuddin burhani <burhani.2016@gmail.com>
2023-08-30 18:14:41 -0700

    New updated project sttings

--- contact_list/urls.py
+++ contact_list/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('contacts.urls')),
     path('admin/', admin.site.urls),
 ]
 
-# urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += staticfiles_urlpatterns()
